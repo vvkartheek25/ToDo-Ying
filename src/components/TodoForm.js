02465f1d828fff2be45b206840fcd31b33159866
Kartheek Vatti <55904811+vvkartheek25@users.noreply.github.com>
2021-05-16 20:34:12 -0700

    Add files via upload

---
+++ src/components/TodoForm.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setInput('');
+  };
+
+
+  return (
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <br/>
+          <br/>
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Save
+          </button>
+          
+          <button onClick={handleChange} className='todo-button cancel'>
+            Cancel
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Add a todo'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Add To List
+          </button>
+        </>
+      )}
+    </form>
+
+  );
+}
+
+export default TodoForm;
